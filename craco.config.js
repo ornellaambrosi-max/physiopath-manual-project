547f1abf558914259429892c2f0e36ec8f867fbb
ornellaambrosi-max <ornellaambrosi@gmail.com>
2025-08-16 13:16:22 +0200

    chore: craco alias + ui components + utils

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+﻿const path = require('path');
+module.exports = {
+  webpack: {
+    alias: { '@': path.resolve(__dirname, 'src') },
+    configure: (config) => {
+      const set = new Set([...(config.resolve.extensions || []), '.jsx', '.js']);
+      config.resolve.extensions = Array.from(set);
+      return config;
+    },
+  },
+};
